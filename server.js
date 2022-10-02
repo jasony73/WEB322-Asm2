95fe96e8773b260ac5631bc7d1fe385083ddbadd
Ka Chun Yuen <kyuen16@myseneca.ca>
2022-10-02 19:16:43 -0400

    initial commit

--- server.js
+++ server.js
@@ -15,19 +15,19 @@ app.use(express.static("static"));
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/blog.html"));
-});
+})
 
 
 // P1.setup another route to listen on /blog
 app.get("/blog", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/blog.html"));
 
-});
+})
 
 app.get("/article", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/read_more.html"));
 
-});
+})
 
 app.get("/registration", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/registration.html"));
@@ -37,11 +37,11 @@ app.get("/registration", function(req,res,next){
 app.get("/login", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/login.html"));
 
-});
+})
 
 app.use((req, res) => {
   res.status(404).send("404 !!! Page Not Found");
-});
+})
 
 
 
