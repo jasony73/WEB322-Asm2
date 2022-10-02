bb0fecbf2fba0639bf8cd593d381060c5eff0a91
Ka Chun Yuen <kyuen16@myseneca.ca>
2022-10-02 19:51:43 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
-var express = require("express");
+var path = require("path");
 
+
+var express = require("express");
+const { nextTick } = require("process");
 var app = express();
 
 var HTTP_PORT = process.env.PORT ||3000;
 
+var routes = require('./views')(app);
 // call this function after the http server starts listening for requests
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 
 }
+
+app.use('/image', express.static(__dirname + '/image'));
+// setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res,next){
   res.sendFile(path.join(__dirname,"/views/blog.html"));
 })
 
-app.configure(function(){
-  app.use('/image', express.static(__dirname + '/image'));
-  // setup a 'route' to listen on the default url path (http://localhost)
-  app.get("/", function(req,res,next){
-    res.sendFile(path.join(__dirname,"/views/blog.html"));
-  })
-  
-  
-  // P1.setup another route to listen on /blog
-  app.get("/blog", function(req,res,next){
-    res.sendFile(path.join(__dirname,"/views/blog.html"));
-  
-  })
-  
-  app.get("/article", function(req,res,next){
-    res.sendFile(path.join(__dirname,"/views/read_more.html"));
-  
-  })
-  
-  app.get("/registration", function(req,res,next){
-    res.sendFile(path.join(__dirname,"/views/registration.html"));
-  
-  });
-  
-  app.get("/login", function(req,res,next){
-    res.sendFile(path.join(__dirname,"/views/login.html"));
-  
-  })
-  
-  app.use((req, res) => {
-    res.status(404).send("404 !!! Page Not Found");
-  })
-  
-  
 
+
+// P1.setup another route to listen on /blog
+app.get("/blog", function(req,res,next){
+  res.sendFile(path.join(__dirname,"/views/blog.html"));
+
+})
+
+app.get("/article", function(req,res,next){
+  res.sendFile(path.join(__dirname,"/views/read_more.html"));
+
+})
+
+app.get("/registration", function(req,res,next){
+  res.sendFile(path.join(__dirname,"/views/registration.html"));
 
 });
+
+app.get("/login", function(req,res,next){
+  res.sendFile(path.join(__dirname,"/views/login.html"));
+
+})
+
+app.use((req, res) => {
+  res.status(404).send("404 !!! Page Not Found");
+})
+
 
 
 
