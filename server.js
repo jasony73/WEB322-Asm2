7c17aa9dffcf637e816e75974dac10be32b831cd
Ka Chun Yuen <kyuen16@myseneca.ca>
2022-10-19 17:15:25 -0400

    innitial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 var HTTP_PORT = process.env.PORT ||8080;
 
-function onHttpSStart(){
+function onHttpStart(){
   console.log("Express http server listening on; " + HTTP_PORT);
 }
 
