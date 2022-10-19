6e3f53ce89eb34f8dbce48694ccf1f58931b0518
Ka Chun Yuen <kyuen16@myseneca.ca>
2022-10-19 16:59:43 -0400

    innitial commit

--- server.js
+++ server.js
@@ -1,53 +1,48 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
 var path = require("path");
 
+var HTTP_PORT = process.env.PORT ||8080;
 
-var express = require("express");
-const { nextTick } = require("process");
-var app = express();
-
-var HTTP_PORT = process.env.PORT ||3000;
-
-var routes = require('./views')(app);
-// call this function after the http server starts listening for requests
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
-
+function onHttpSStart(){
+  console.log("Express http server listening on; " + HTTP_PORT);
 }
 
-app.use('/image', express.static(__dirname + '/image'));
+
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res,next){
+app.get("/", function(req,res){
   res.sendFile(path.join(__dirname,"/views/blog.html"));
 })
 
 
 
 // P1.setup another route to listen on /blog
-app.get("/blog", function(req,res,next){
+app.get("/blog", function(req,res){
   res.sendFile(path.join(__dirname,"/views/blog.html"));
 
 })
 
-app.get("/article", function(req,res,next){
+app.get("/article", function(req,res){
   res.sendFile(path.join(__dirname,"/views/read_more.html"));
 
 })
 
-app.get("/registration", function(req,res,next){
+app.get("/registration", function(req,res){
   res.sendFile(path.join(__dirname,"/views/registration.html"));
 
 });
 
-app.get("/login", function(req,res,next){
+app.get("/login", function(req,res){
   res.sendFile(path.join(__dirname,"/views/login.html"));
 
-})
+});
 
 app.use((req, res) => {
   res.status(404).send("404 !!! Page Not Found");
-})
+});
 
-
+app.use(express.static("static"));
 
 
 // setup http server to listen on HTTP_PORT
